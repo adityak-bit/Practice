3dc5c8ee22cee5016b41b38116367b39bfaf3c10
adityak-bit <adityajsr1998@gmail.com>
2020-09-25 11:17:19 +0530

    commit UC6

--- Practice/src/com/Practice/Hello.java
+++ Practice/src/com/Practice/Hello.java
@@ -2,7 +2,7 @@ package com.Practice;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello football ");
+		System.out.println("Hello football and switched to branch UC6 ");
 	}
 
 }
